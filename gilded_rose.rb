f20d2eda36e5c384475f8510f9df65b63bebc9c1
Sofi <chaussettefurtive@outlook.fr>
2019-02-13 21:27:04 +0100

    Add our exercice

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,52 +5,72 @@ class GildedRose
   end
 
   def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+    
   end
+
+  def normal()
+    [-1, -1]
+  end
+
+  def conjured(q, s)
+  
+  end
+
+  def aged_brie()
+
+  end
+
+  def sulfuras()
+  end
+
+  def update_quality()
+    
+  end
+
+  # def update_quality()
+  #   @items.each { |item|
+  #     mult = 1
+  #     name = item.name.downcase
+  #     if name.include? "conjured"
+  #       mult = 2
+  #     end
+
+  #     # Decrease number of days
+  #     item.sell_in -= 1
+
+  #     if not name.include? "sulfuras"
+  #       # Deadline
+  #       if item.sell_in < 0 
+  #         if name.include? "backstage passes"
+  #           item.quality = 0
+  #         else
+  #           item.quality -= 2 * mult
+  #         end
+  #       else
+  #         if name.include? "aged brie"
+  #           item.quality += 1
+  #         elsif name.include? "backstage passes"
+  #           item.quality += 1
+  #           if item.sell_in <= 10
+  #             item.quality += 1
+  #             if item.sell_in <= 5
+  #               item.quality += 1
+  #             end
+  #           end
+  #         else
+  #           item.quality -= 1 * mult
+  #         end
+  #       end
+  #     end
+    
+  #     if item.quality < 0
+  #       item.quality = 0
+  #     elsif item.quality > 50
+  #       item.quality = 50
+  #     end
+      
+  #   }
+  # end
 end
 
 class Item
